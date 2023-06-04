9332c400199820debe5f90784c73d6b808ce2473
unknown <01168941@pw.edu.pl>
2023-06-04 11:45:58 +0200

    Fix Docker Ports

--- majumi.ClientApp.BlazorServer/Program.cs
+++ majumi.ClientApp.BlazorServer/Program.cs
@@ -24,5 +24,5 @@ public class Program
 	}
 
 	public static IHostBuilder CreateHostBuilder(string[] args) =>
-	  Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+	  Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); webBuilder.UseUrls("http://*:5100"); });
 }
