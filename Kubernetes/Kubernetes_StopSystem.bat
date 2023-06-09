c85dfc3d9c79e56ef6274e7b7b77ae6ea0f4f82b
K3rne132 <mtjurczak@gmail.com>
2023-06-09 17:04:18 +0200

    K8s finish

--- Kubernetes/Kubernetes_StopSystem.bat
+++ Kubernetes/Kubernetes_StopSystem.bat
@@ -1,14 +1,20 @@
 echo off
 kubectl delete service clientapp
+kubectl delete service mechanicapp
 kubectl delete service clientappservice
+kubectl delete service mechanicappservice
 kubectl delete service carsdataservice
 kubectl delete service clientsdataservice
+kubectl delete service mechanicsdataservice
 kubectl delete service visitsdataservice
 
 kubectl delete deployment clientapp-deploy
+kubectl delete deployment mechanicapp-deploy
 kubectl delete deployment clientappservice-deploy
+kubectl delete deployment mechanicappservice-deploy
 kubectl delete deployment carsdataservice-deploy
 kubectl delete deployment clientsdataservice-deploy
+kubectl delete deployment mechanicsdataservice-deploy
 kubectl delete deployment visitsdataservice-deploy
 pause
 
